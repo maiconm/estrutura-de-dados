51b56eed0c0f2a4f8d4b1d607d04d8a9c0d0d1b8
Maicon Andraski <maiconm23@gmail.com>
2019-11-08 06:41:30 -0300

    Registro com os dados gerais das disciplinas de um sistema de administração escolar

---
+++ 5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct registro{
+	char nomeDisciplina[30];
+	char descricao[150];
+	int frequencia;
+	int notaMin;	
+};
+
+struct cadastro[300]{
+	char disciplina[40];
+	int frequencia;
+	int nota;
+};
+int main(int argc, char *argv[]) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
